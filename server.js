7cdedeeaa0005d2b9bd5959d987034ffd2bfc717
barinder <barindersingh1997@gmail.com>
2025-09-02 16:16:48 +0530

    vcl

--- server.js
+++ server.js
@@ -1,3 +1,32 @@
+// const dotenv = require("dotenv");
+// const express = require("express");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const connectDB = require("./config/db");
+// const propertyRoutes = require("./routes/propertyRoutes.js");
+
+// dotenv.config();
+// connectDB();
+// console.log("Loaded MONGO_URI =", process.env.MONGO_URI);
+
+// const app = express();
+// app.use(cors());
+// app.use(bodyParser.json());
+
+
+// console.log("MONGO_URI =", process.env.MONGO_URI);
+
+
+// // Routes
+// app.use("/api/properties", propertyRoutes);
+
+// // Start server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(` Server running on http://localhost:${PORT}`);
+// });
+
+
 const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
@@ -7,21 +36,13 @@ const propertyRoutes = require("./routes/propertyRoutes.js");
 
 dotenv.config();
 connectDB();
-console.log("Loaded MONGO_URI =", process.env.MONGO_URI);
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
-console.log("MONGO_URI =", process.env.MONGO_URI);
-
-
 // Routes
 app.use("/api/properties", propertyRoutes);
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+
+module.exports = app;
